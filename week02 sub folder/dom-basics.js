8f044b013a660e6cce70c937a9a86017e7ccc357
izekorphilip <izekorphilip5@gmail.com>
2023-09-28 04:16:08 +0100

    A new push

---
+++ week02 sub folder/dom-basics.js
@@ -0,0 +1,18 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "images/pictur.JPG");
+newImage.setAttribute("alt", "Our plans");
+document.body.appendChild(newImage);
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
+
+
+const newSection = document.createElement("section");
+newSection.innerHTML = "<h2>CSE 121b</h2><p>Welcome to Javascript Language</p>";
+document.body.appendChild(newSection);
